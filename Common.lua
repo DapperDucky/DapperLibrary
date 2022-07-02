f9d113dff2db6cff1e39729f115f4204f712986c
Rubber Ducky <104643242+DapperDucky@users.noreply.github.com>
2022-07-02 15:00:50 -0500

    Init 2: The Real Init

---
+++ Common.lua
@@ -0,0 +1,12 @@
+
+AlienCores = {}
+    AlienCores[1]  = {Name = "AC1",  x = 13666000, y = 1622000, z = -46840000}
+    AlienCores[2]  = {Name = "AC2",  x = -45534000, y = -46878000, z = -739465}
+    AlienCores[3]  = {Name = "AC3",  x = 58166000, y = -52378000, z = -739465}
+    AlienCores[4]  = {Name = "AC4",  x = 33946000, y = 71381990, z = 28850000}
+    AlienCores[5]  = {Name = "AC5",  x = -64334000, y = 55522000, z = -14400000}
+    AlienCores[6]  = {Name = "AC6",  x = 81766000, y = 16912000, z = 23860000}
+    AlienCores[7]  = {Name = "AC7",  x = 48566000, y = 19622000, z = 101000000}
+    AlienCores[8]  = {Name = "AC8",  x = -73134000, y = 18722000, z = -93700000}
+    AlienCores[9]  = {Name = "AC9",  x = -145634000, y = -10578000, z = -739465}
+    AlienCores[10] = {Name = "AC10", x = 966000, y = -149278000, z = -739465}
